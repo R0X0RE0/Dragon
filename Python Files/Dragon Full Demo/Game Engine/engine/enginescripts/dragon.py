d7b92eee9165b65d56692fe7db5e631826785b27
R0X0RE0 <44986343+R0X0RE0@users.noreply.github.com>
2019-02-03 08:25:27 -0500

    Add files via upload

---
+++ Python Files/Dragon Full Demo/Game Engine/engine/enginescripts/dragon.py
@@ -0,0 +1,39 @@
+# Game engine made by Rox (Dylan Hawley)
+# Pygame Authors (https://github.com/pygame/pygame/graphs/contributors)
+# Started on Jan 31, 2019
+# Demo started on Feb 1, 2019
+# Engine started on Feb 2, 2019
+# GitHub Repository ()
+import pygame
+import pygame.locals
+import pydragon
+pygame.init()
+def toggle_fullscreen(screen_res):
+    if toggle_fullscreen.full:
+        screen_res = pygame.display.set_mode((w,h),pygame.FULLSCREEN)
+    else:
+        screen_res = pygame/display.set_mode((w,h))
+    toggle_fullscreen.full = not toggle_fullscreen.full
+    return screen_res
+toggle_fullscreen.full = False
+class default:
+    resolution = 1280,720
+w,h = default.resolution
+screen_res = pygame.display.set_mode((w,h),pygame.FULLSCREEN)
+class Control:
+    def update():
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_TAB:
+                        toggle_fullscreen(screen_res)
+Running = True
+while Running:
+    keypress = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            Running = False
+        elif event.type == pygame.KEYDOWN:
+            if (keypress[pygame.K_RALT] or keypress[pygame.K_LALT]) and keypress[pygame.K_F4]:
+                Running = False
+    Control.update()
+pygame.quit
